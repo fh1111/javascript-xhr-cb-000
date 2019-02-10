6119abea64ea0bf5a3d2ee3df1d0d57311cd1dfe
Fer Hernandez <fh.1111@gmail.com>
2019-02-10 23:36:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 function showRepositories(event, data) {
-  //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText);
-
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos.map(r => "<li>" + r.name + "</li>").join("")}</ul>`;
-
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        "<li>" +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join("")}</ul>`;
   document.getElementById("repositories").innerHTML = repoList;
 }
-
 
 function getRepositories() {
   const req = new XMLHttpRequest();
